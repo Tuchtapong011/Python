871f0aa9eb14df987266bd67d6e8da98394454d8
Tuchtapong011 <664230011@webmail.npru.ac.th>
2025-11-19 15:28:14 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,20 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 st.header('Tuchtapong')
 st.image("./img/images.jpg")
 col1, col2, col3 = st.columns(3)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
