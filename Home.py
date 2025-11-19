c16aeba4aa502303fd3a5a793681ca7e0884896e
Tuchtapong011 <664230011@webmail.npru.ac.th>
2025-11-19 15:26:03 +0700

    Update Home.py

---
+++ Home.py
@@ -0,0 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.header('Tuchtapong')
+st.image("./img/images.jpg")
+col1, col2, col3 = st.columns(3)
